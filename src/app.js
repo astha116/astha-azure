706befcded276ccf9ca41d8351e9a713f97b3a36
AsthaJain <asthajain116@gmail.com>
2025-08-23 19:01:15 +0000

    added custom message

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello DevOps!' });
+  res.json({  message: process.env.CUSTOM_MESSAGE ||  'Hello DevOps!' });
 });
 
 app.get('/health', (req, res) => {
